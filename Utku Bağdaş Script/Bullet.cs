e47dd5629a222690fb8af78db264715d7e4e6c45
Utku <utku.bagdas@gmail.com>
2020-12-18 16:35:35 +0300

    Utku Bağdaş Script

---
+++ Utku Bağdaş Script/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour, IEatable
+{
+    public void ChangeToMainText()
+    {
+        OnInteraction.text = "Mermi toplamak için E'ye basınız.";
+    }
+
+    public void Dispose()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Use()
+    {
+        Debug.Log("bullet collected");
+        GameManager.Instance.bulletcount += 1;
+        Debug.Log("Bullet count : " + GameManager.Instance.bulletcount);
+        Dispose();
+    }
+}
---
+++ Utku Bağdaş Script/OnInteraction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OnInteraction : MonoBehaviour
+{
+    public static string text = "";
+    public Text text2;
+    
+    void Update()
+    {
+        Ray point = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+
+            if (Physics.Raycast(point, out RaycastHit hit)) 
+            {
+                IEatable eatable = hit.collider.GetComponent<IEatable>();
+                if(eatable != null)
+                {
+                    eatable.Use();
+                   
+                    
+                }
+            }
+        }
+
+       else if(Physics.Raycast(point, out RaycastHit hit))
+        {
+            IEatable eatable = hit.collider.GetComponent<IEatable>();
+
+            if(eatable != null)
+            {
+                eatable.ChangeToMainText();
+                text2.text = text;
+            }
+            else
+            {
+
+                text = "";
+                text2.text = text;
+            }
+
+        }
+    }
+}
